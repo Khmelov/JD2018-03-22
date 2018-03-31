ee335129a7af7969b3a01b09be1e82beb74e4a3b
Olga_Matyuta <tiefatem@gmail.com>
2018-03-31 12:08:48 +0300

    jd01...05

---
+++ src/by/it/matyuta/jd01/Hello.java
@@ -0,0 +1,14 @@
+package by.it.matyuta.jd01;
+
+public class Hello {
+
+    private String slogan = "Hello, world!";
+
+    void setSlogan(String slogan){
+        this.slogan = slogan;
+    }
+
+    void printSlogan(){
+        System.out.println(slogan);
+    }
+}
