ea446b6278aac2b144a88ec409bd5cecc2a08a73
Michael <mishanya.v91@mail.ru>
2018-03-31 12:09:47 +0300

    first commit and push. dj01...05 TaskA

---
+++ src/by/it/verishko/jd01_02/TaskA.java
@@ -0,0 +1,52 @@
+package by.it.verishko.jd01_02;
+
+import java.util.Scanner;
+
+public class TaskA {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] mas = new int[10];
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = scanner.nextInt();
+        }
+        step1(mas);
+        step2(mas);
+        step3(mas);
+
+
+    }
+
+    private static void step1(int[] mas) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int element : mas) {
+            if (min > element) min = element;
+            if (max < element) max = element;
+        }
+        System.out.println(min + " " + max);
+    }
+
+    private static void step2(int[] mas) {
+        double avg = 0;
+        for (int element : mas) {
+            avg = avg + element;
+        }
+        avg = avg / mas.length;
+        for (int element : mas) {
+            if (element < avg) {
+                System.out.print(element + " ");
+            }
+        }
+    }
+
+    private static void step3(int[] mas) {
+        int min = Integer.MAX_VALUE;
+        for (int element : mas) {
+            if (min > element) min = element;
+        }
+        for (int i = mas.length - 1; i >= 0; i--) {
+            if (mas[i] == min)
+                System.out.print(i + " ");
+        }
+    }
+}
---
+++ src/by/it/verishko/jd01_04/Helper.java
@@ -0,0 +1,56 @@
+package by.it.verishko.jd01_04;
+
+public class Helper {
+    static double findMin(double[] arr) {
+        if (0 == arr.length) {
+            return Double.MIN_VALUE;
+        } else {
+            double min = arr[0];
+            for (double m : arr) {
+                if (min > m) min = m;
+            }
+            return min;
+        }
+    }
+
+    static double findMax(double[] arr) {
+        if (0 == arr.length) {
+            return Double.MAX_VALUE;
+        } else {
+            double max = arr[0];
+            for (double m : arr) {
+                if (max < m) max = m;
+            }
+            return max;
+        }
+    }
+
+    static void sort(double[] arr) {
+        for (int i = 0; i < arr.length - 1; i++)
+            for (int j = i + 1; j < arr.length; j++)
+                if ((arr[i] > arr[j]) || (!(arr[i] < arr[j]))) {
+                    double m = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = m;
+                }
+    }
+
+    static double[] mul(double[][] matrix, double[] vector) {
+        double[] z = new double[matrix.length];
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = 0; j < vector.length; j++)
+                z[i] = z[i] + matrix[i][j] * vector[j];
+        return z;
+    }
+
+    static double[][] mul(double[][] matrixLeft, double[][] matrixRight) {
+        double[][] r = new double[matrixLeft.length][matrixRight[0].length];
+        for (int i = 0; i < matrixLeft.length; i++)
+            for (int j = 0; j < matrixRight[0].length; j++)
+                for (int k = 0; k < matrixRight.length; k++)
+                    r[i][j] = r[i][j] + matrixLeft[i][k] * matrixRight[k][j];
+        return r;
+    }
+}
+
+
