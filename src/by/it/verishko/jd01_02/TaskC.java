8c86f28184ed0a6e96445ccc0b4e6e2727f593c1
Michael <mishanya.v91@mail.ru>
2018-03-31 12:09:47 +0300

    first commit and push. dj01...05 TaskA

---
+++ src/by/it/verishko/jd01_02/TaskC.java
@@ -0,0 +1,129 @@
+package by.it.verishko.jd01_02;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class TaskC {
+
+
+    public static void main(String[] args) {
+        int[][] m4 = {{1, -2, -2, 6}, {-1, 2, -2, 2}, {-2, -2, -6, -2}, {1, 2, -2, 6}};
+        step1(9);
+        step2(m4);
+        int[][] mas2 = step3(m4);
+    }
+
+    static int[][] step1(int n) {
+        Random random = new Random();
+        boolean positive = false;
+        boolean negative = false;
+
+        int[][] res = new int[n][n];
+        while (!positive || !negative)
+            for (int i = 0; i < res.length; i++) {
+                for (int j = 0; j < res[i].length; j++) {
+                    int k = random.nextInt(2 * n + 1) - n;
+                    if (k == -n) negative = true;
+                    if (k == n) positive = true;
+                    res[i][j] = k;
+                }
+            }
+        for (int[] row : res) {
+            System.out.println(Arrays.toString(row));
+        }
+        return res;
+    }
+
+    static int step2(int[][] mas) {
+        int res = 0;
+        for (int[] row : mas) {
+            int i = 0;
+            while (i < row.length && row[i] <= 0) i++;
+            i++;
+            while (i < row.length && row[i] <= 0) {
+                res += row[i];
+                i++;
+            }
+        }
+        System.out.println(res);
+        return res;
+    }
+
+    static int[][] step3(int[][] mas) {
+        if (mas.length == 0)
+            return new int[0][0];
+
+        int rows = mas.length;
+        int cols = mas[0].length;
+        boolean[] delRow = new boolean[rows];
+        boolean[] delCol = new boolean[cols];
+        int max = Integer.MIN_VALUE;
+        for (int[] row : mas) {
+            for (int element : row) {
+                if (max < element) max = element;
+            }
+        }
+
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas[i].length; j++)
+            if (mas[i][j]==max){
+                if (!delRow[i]) {
+                    delRow[i] = true;
+                    rows--;
+                }
+                if (!delCol[j]) {
+                    delCol[j] = true;
+                    cols--;
+                }
+            }
+        }
+
+        int[][] res = new int[rows][cols];
+        int ii = 0;
+        for (int i = 0; i < mas.length; i++) {
+            if (!delRow[i]) {
+                int jj = 0;
+                for (int j = 0; j < mas[i].length; j++) {
+                    if (!delCol[j]) {
+                        res[ii][jj] = mas[i][j];
+                        jj++;
+                    }
+                }
+                ii++;
+            }
+        }
+        for (int[] row : res) {
+            System.out.println(Arrays.toString(row));
+        }
+//        System.out.println(res);
+        return res;
+    }
+}
+
+
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int[][] mas = new int[n][n];
+//        step1(mas);
+//    }
+//
+//
+//    private static void step1(int[][] mas) {
+//        Random rnd = new Random();
+//        int j = 0, i = 0;
+//        if (i != mas.length) {
+//            for (i = 0; i < mas.length; i++) {
+//                for (j = 0; j < mas[i].length; j++) {
+//                    mas[i][j] = rnd.nextInt(mas.length * 2 + 1) - mas.length;
+//                }
+//            }
+//            for (i = 0; i < mas.length; i++, System.out.println()) {
+//                for (j = 0; j < mas[i].length; j++) {
+//                    System.out.print(mas[i][j] + " ");
+//                }
+//            }
+//        }
+////        System.out.print(mas[i][j] + " ");
+//    }
