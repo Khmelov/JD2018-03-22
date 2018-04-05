eb7542b70befacc50cbca84d5901fb3fa465e392
svolchek <6252750@gmail.com>
2018-04-05 12:48:41 +0300

    HomeTask5

---
+++ src/by/it/volchek/jd01_07/Scalar.java
@@ -0,0 +1,26 @@
+package by.it.volchek.jd01_07;
+
+/**
+ * Created by volchek on 05.04.18.
+ */
+public class Scalar extends AbstractVar{
+    private double value;
+
+    Scalar (double value){
+        this.value = value;
+
+    }
+    Scalar (String str){
+        this.value = Double.parseDouble(str);
+
+    }
+    Scalar (Scalar otherScalar){
+        this.value=otherScalar.value;
+
+    }
+
+    @Override
+    public String toString (){
+        return Double.toString(value);
+    }
+}
