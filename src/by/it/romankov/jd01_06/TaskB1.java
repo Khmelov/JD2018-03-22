bf4d5d4f81a6b461c148d6f023b30728ec5fd95a
nikitaromankov <nikitarom2@gmail.com>
2018-04-05 11:32:52 +0300

    jd01...05 TaskA, TaskB

---
+++ src/by/it/romankov/jd01_06/TaskB1.java
@@ -0,0 +1,29 @@
+package by.it.romankov.jd01_06;
+
+public class TaskB1 {
+    public static void main(String[] args) {
+        char[] o = {'ё', 'у', 'е', 'ы', 'а', 'о', 'э', 'я', 'и', 'ю'};
+        String s = Poem.text.replaceAll(",", "").replaceAll("\\.", "").replaceAll(":", "");
+
+        String[] ss = s.split("\n");
+        for ( int k = 0; k < ss.length; k++ ) {
+            String[] str =ss[k].split(" ");
+            for ( int i = 0; i < str.length; i++ ) {
+                char[] c = str[i].trim().toCharArray();
+                boolean firstSoglasnaya = true;
+                boolean lastGlasnaya = false;
+                for ( int j = 0; j < o.length; j++ ) {
+                    if (o[j] == c[0]) {
+                        firstSoglasnaya = false;
+                    } else if (o[j] == c[c.length - 1]) {
+                        lastGlasnaya = true;
+                    }
+                }
+                if (firstSoglasnaya && lastGlasnaya) {
+                    System.out.println(str[i]);
+                }
+            }
+        }
+    }
+}
+
