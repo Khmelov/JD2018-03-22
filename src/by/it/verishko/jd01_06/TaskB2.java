469fa650fc69ee0b25ab80a4067a1ecb056ba485
Michael <mishanya.v91@mail.ru>
2018-04-06 13:22:41 +0300

    jd01_06 TaskB2

--- src/by/it/verishko/jd01_06/TaskB2.java
+++ src/by/it/verishko/jd01_06/TaskB2.java
@@ -4,64 +4,29 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.regex.Pattern;
 
-//public class TaskB2 {
-//    public static void main(String[] args) {
-//        Pattern pattern = Pattern.compile("[.?!][\\n]");
-//        String[] lines = pattern.split(Poem.text);
-//        for (int i = 0; i < lines.length; i++) {
-//            lines[i] = lines[i].replaceAll("[\\s,;:.!?-]+|\\n", " ");
-//            lines[i] = lines[i].trim();
-//        }
-//        Comparator<String> stringLengthComparator = new StringLengthSort();
-//        Arrays.sort(lines, stringLengthComparator);
-//        for (String element : lines) {
-//            System.out.println(element);
-//        }
-//    }
-//
-//    static class StringLengthSort implements Comparator<String> {
-//        public int compare(String s1, String s2) {
-//            return Integer.compare(s1.length(), s2.length());
-//        }
-//    }
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 public class TaskB2 {
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile("[.?!][\\n]");
         String[] lines = pattern.split(Poem.text);
         for (int i = 0; i < lines.length; i++) {
-            lines[i] = lines[i].trim();
-            lines[i] = lines[i].replaceAll("[\\s,;:.!?-]+|\\n", " ");
+            lines[i] = lines[i].replaceAll("[\\s,;:.!?-]+|\\n", " ").trim();
+            System.out.println(lines[i]);
         }
 
+        Comparator<String> stringLengthComparator = new StringLengthSort();
+        Arrays.sort(lines, stringLengthComparator);
+        for (String element : lines) {
+            System.out.println(element);
+        }
+    }
+
+    static class StringLengthSort implements Comparator<String> {
+        public int compare(String s1, String s2) {
+            return Integer.compare(s1.length(), s2.length());
+        }
     }
 }
-
-
-
-
-
 
 
 
