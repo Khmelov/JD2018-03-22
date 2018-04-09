6379f5105eb4d562a5ef1e2249d2fd86f07873e2
eukovko <eu.kovko@gmail.com>
2018-04-09 12:53:22 +0300

    jd01_08 A,B,C + oop

--- src/by/it/kovko/jd01_08/opp/Movie.java
+++ src/by/it/kovko/jd01_08/opp/Movie.java
@@ -1,9 +1,12 @@
 package by.it.kovko.jd01_08.opp;
 
 public interface Movie {
-
     void rate(int r);
     void buy();
-    void comment();
-    void recommend();
+    void rent(int days);
+    void comment(String com);
+    void recommend(int userId);
+    void markAsViewed();
+    void adToWishList();
+
 }
