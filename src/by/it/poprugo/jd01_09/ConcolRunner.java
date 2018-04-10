4caea3db01be2030b5c5d64cb13111c6c1a1dc4a
AlenaVP <poprugo@gmail.com>
2018-04-10 12:03:49 +0300

    All old until C of jd01_05

---
+++ src/by/it/poprugo/jd01_09/ConcolRunner.java
@@ -0,0 +1,16 @@
+package by.it.poprugo.jd01_09;
+
+import java.util.Scanner;
+
+public class ConcolRunner {
+    public static void main(String[] args) {
+        Printer printer=new Printer();
+        Parser parser=new Parser();
+        Scanner scanner=new Scanner(System.in);
+        String line;
+        while (!(line=scanner.nextLine()).equals("end")) {
+            Var result=parser.calc(line);
+            printer.print(result);
+        }
+    }
+}
