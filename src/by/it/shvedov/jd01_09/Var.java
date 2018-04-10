529f3de9c8ca347f7e13ae6f29400252d2ddc443
Шведов <alexshvedov1997@gmail.com>
2018-04-10 12:00:22 +0300

    jd01_09 доделать

---
+++ src/by/it/shvedov/jd01_09/Var.java
@@ -0,0 +1,28 @@
+package by.it.shvedov.jd01_09;
+
+abstract class Var implements Operation {
+
+    @Override
+    public Var add(Var other) {
+        System.out.println("Операция сложения"+this+"+"+other+"невозможна");
+        return null;
+    }
+
+    @Override
+    public Var sub(Var other) {
+        System.out.println("Операция вычитания"+this+"-"+other+"невозможна");
+        return null;
+    }
+
+    @Override
+    public Var mul(Var other) {
+        System.out.println("Операция умножения"+this+"*"+other+"невозможна");
+        return null;
+    }
+
+    @Override
+    public Var div(Var other) {
+        System.out.println("Операция деления"+this+"/"+other+"невозможна");
+        return null;
+    }
+}
