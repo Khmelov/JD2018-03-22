b071350ae2407f1314206034ee03b6fdc656b198
numanmybro <moralofinsanity@gmail.com>
2018-04-10 11:04:25 +0300

    jd01_01-07

---
+++ src/by/it/pashkevich/jd01_08/Vector.java
@@ -0,0 +1,35 @@
+package by.it.pashkevich.jd01_08;
+
+public class Vector extends Var {
+
+    private double[] array;
+
+    Vector(double[] value) {
+        this.array = value;
+    }
+
+    Vector(Vector vector) {
+        this.array = vector.array;
+
+    }
+
+    Vector(String strVector) {
+        String[] s = strVector.substring(1, strVector.length() - 1).split(",");
+        array = new double[s.length];
+        for (int i = 0; i <s.length ; i++) {
+            array[i] = Double.parseDouble(s[i]);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <array.length-1 ; i++) {
+            sb.append(array[i]).append(", ");
+        }
+        sb.append(array[array.length-1]);
+        return "{" + sb.toString() + "}";
+
+    }
+}
+
