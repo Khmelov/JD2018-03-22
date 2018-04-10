802929b064a59b6d229990ddbb0604b6fb98cac9
Michael <mishanya.v91@mail.ru>
2018-04-10 12:00:11 +0300

    jd01_09 TaskA, TaskB

---
+++ src/by/it/verishko/jd01_09/Matrix.java
@@ -0,0 +1,29 @@
+package by.it.verishko.jd01_09;
+
+import java.util.Arrays;
+
+public class Matrix {
+    private double[][] matrix;
+
+//    Matrix(double[][] value) {
+//        this.value = value;
+//    }
+
+//    Matrix(Matrix matrix) {
+//        this.value = matrix.value;
+//    }
+
+    Matrix(String strMatrix) {
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb=new StringBuilder();
+
+
+        return "Matrix{" +
+                "matrix=" + Arrays.toString(matrix) +
+                '}';
+    }
+}
---
+++ src/by/it/verishko/jd01_09/Var.java
@@ -0,0 +1,43 @@
+package by.it.verishko.jd01_09;
+
+abstract class Var implements Operation {
+
+    static Var createVar(String strVar) {
+        if (strVar.matches(Patterns.SCALAR))
+            return new Scalar(strVar);
+        if (strVar.matches(Patterns.VECTOR))
+            return new Vector(strVar);
+//        if (strVar.matches(Patterns.MATRIX))
+//            return new Matrix(strVar);
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Это класс Var{}";
+    }
+
+    @Override
+    public Var add(Var other) {
+        System.out.printf("Операция сложения %s+%s невозможна\n", this, other);
+        return null;
+    }
+
+    @Override
+    public Var sub(Var other) {
+        System.out.printf("Операция вычитания %s-%s невозможна\n", this, other);
+        return null;
+    }
+
+    @Override
+    public Var mul(Var other) {
+        System.out.printf("Операция умножения %s*%s невозможна\n", this, other);
+        return null;
+    }
+
+    @Override
+    public Var div(Var other) {
+        System.out.printf("Операция деления %s/%s невозможна\n", this, other);
+        return null;
+    }
+}
