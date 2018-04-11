dc1d1025bd680c0acb5943474813ae08064658f3
svolchek <6252750@gmail.com>
2018-04-11 20:55:20 +0300

    jd01_07

---
+++ src/by/it/volchek/jd01_09/Printer.java
@@ -0,0 +1,11 @@
+package by.it.volchek.jd01_09;
+
+/**
+ * Created by volchek on 10.04.18.
+ */
+public class Printer {
+    void print(Var var){
+        if (var!=null)
+            System.out.println(var);
+    }
+}
