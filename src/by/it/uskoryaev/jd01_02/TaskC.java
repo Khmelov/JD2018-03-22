cbd3f54f9f92b6463f6b3c1c6e41c83900803c33
ProgrammStart <ProgrammStart625@gmail.com>
2018-04-12 08:20:18 +0300

    Task 8 part a,b,c(нет матиматической части)

--- src/by/it/uskoryaev/jd01_02/TaskC.java
+++ src/by/it/uskoryaev/jd01_02/TaskC.java
@@ -6,21 +6,45 @@
 //    public static void main(String[] args) {
 //        Scanner scanner = new Scanner(System.in);
 //        int n = scanner.nextInt();
+//        int [] [] mas = new int[n][n];
 //
-//        step1();
-//        step2();
-//        step3();
+//        step1(n);
+//        step2(mas);
+//        step3(mas);
 //
-//
-//    }
-//    int[ ][ ] step1(int n){
-//    }
-//
-//    int step2(int[ ][ ] mas){
 //
 //    }
+//    static int[ ][ ] step1(int n) {
+//        boolean inspection=false;
+//        int[][] mas = new int[n][n];
+//        do {
+//            for (int i = 0; i < mas.length; i++) {
+//                for (int j = 0; j < mas[i].length; j++) {
+//                    int numberRandom = (int) Math.random() * n - n;
+//                    if (mas[i][j] == numberRandom){
+//                        inspection = true;
+//                    }else {
+//                        inspection = false;
+//                    }
+//                }
+//            }
+//        }while (inspection){
+//            for (int i = 0; i <mas.length ; i++) {
+//                for (int j = 0; j <mas.length ; j++) {
+//                    System.out.print(mas[i][j]+" ");
+//                }
+//                System.out.println();
+//            }
+//            return mas;
+//        }
+//    }
 //
-//    int[ ][ ] step3(int[ ][ ] mas){
+//
+//    static int step2(int[ ][ ] mas){
+//
+//    }
+//
+//    static int[ ][ ] step3(int[ ][ ] mas){
 //
 //    }
 //
--- src/by/it/uskoryaev/jd01_08/oop/SimensMobile.java
+++ src/by/it/uskoryaev/jd01_08/oop/SimensMobile.java
@@ -1,6 +1,38 @@
 package by.it.uskoryaev.jd01_08.oop;
 
-abstract class SimensMobile implements Mobile{
+class SimensMobile extends Model implements Mobile{
+    @Override
+    public boolean off() {
+        return false;
+    }
 
+    @Override
+    public boolean on() {
+        return false;
+    }
 
+    @Override
+    public int levelVolume() {
+        return 0;
+    }
+
+    @Override
+    public int zvonok() {
+        return 0;
+    }
+
+    @Override
+    void playMusic() {
+        super.playMusic();
+    }
+
+    @Override
+    void showPichers() {
+        super.showPichers();
+    }
+
+    @Override
+    void onFonar() {
+        super.onFonar();
+    }
 }
