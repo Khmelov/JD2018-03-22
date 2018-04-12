d2d604e022d975da1fc198e9483e67e5ec32e208
SGolovach <sergei.golovach90@gmail.com>
2018-04-12 11:31:12 +0300

    jd01_09 Task A,B,C

--- src/by/it/sgolovach/jd01_09/Patterns.java
+++ src/by/it/sgolovach/jd01_09/Patterns.java
@@ -3,8 +3,8 @@ package by.it.sgolovach.jd01_09;
 abstract class Patterns {
 
     static final String OPERATION = "[-+*/]";
-    static final String SCALAR = "-?[0-9]+\\.?[0-90]*";
-    static final String VECTOR = "\\{((-?[0-9]+\\.?[0-90]*),?)+}";
-    static final String MATRIX = "\\{(\\{((-?[0-9]+\\.?[0-90]*),?)+},?)+}";
+    static final String SCALAR = "-?[0-9]+\\.?[0-9]*";
+    static final String VECTOR = "\\{((-?[0-9]+\\.?[0-9]*),?)+}";
+    static final String MATRIX = "\\{(\\{((-?[0-9]+\\.?[0-9]*),?)+},?)+}";
 
 }
