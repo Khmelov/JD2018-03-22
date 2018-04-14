d800915dff21fac2187e5376627d63953397329c
svolchek <6252750@gmail.com>
2018-04-14 06:55:36 +0300

    jd01_07

---
+++ src/by/it/volchek/jd01_10/BeanTester.java
@@ -0,0 +1,36 @@
+package by.it.volchek.jd01_10;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.lang.*;
+
+
+/**
+ * Created by volchek on 12.04.18.
+ */
+public class BeanTester {
+    public static void main(String[] args) {
+        Class<?> b = Bean.class;
+        Method[] methods = b.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(Param.class)) {
+                try {
+                    Object o;
+                    o = b.getDeclaredConstructor().newInstance();
+
+                    Object[] paramV = {method.getAnnotation(Param.class).a(), method.getAnnotation(Param.class).b()};
+                    System.out.println(method.getName() + " " + method.invoke(o, paramV));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+
+            }
+        }
+    }
+}
+
+
+
+
