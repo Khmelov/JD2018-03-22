8ac1f04d434552fd1b8f51ca708ead5997c139fe
ProgrammStart <ProgrammStart625@gmail.com>
2018-04-14 09:40:17 +0300

    task 10 a,b

--- src/by/it/uskoryaev/jd01_02/TaskC.java
+++ src/by/it/uskoryaev/jd01_02/TaskC.java
@@ -1,49 +1,48 @@
-//package by.it.uskoryaev.jd01_02;
-//
-//import java.util.Scanner;
-//
-//public class TaskC {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        int n = scanner.nextInt();
-//        int [] [] mas = new int[n][n];
-//
-//        step1(n);
-//        step2(mas);
-//        step3(mas);
-//
-//
-//    }
-//    static int[ ][ ] step1(int n) {
-//        boolean max = false;
-//        boolean min = false;
-//        int [][] mas = new int[n][n];
-//        do {
-//            for (int i = 0; i < mas.length; i++) {
-//                for (int j = 0; j < mas.length; j++) {
-//                    mas[i][j] = (int) Math.random() * (2 * n + 1) - n;
-//                    if (mas[i][j] == n) max = true;
-//                    if (mas[i][j] == -n) min = true;
-//                }
-//            }
-//        }while (!max && !min) {
-//            for (int i = 0; i < mas.length; i++) {
-//                for (int j = 0; j < mas.length; j++) {
-//                    System.out.print(mas[i][j] + " ");
-//                }
-//                System.out.println();
-//            }
-//        }
-//        return mas;
-//    }
-//
-//    static int step2(int[ ][ ] mas){
-//        return 0;
-//
-//    }
-//
-//    static int[ ][ ] step3(int[ ][ ] mas){
-//return null;
-//    }
-//
-//}
+package by.it.uskoryaev.jd01_02;
+
+import java.util.Scanner;
+
+public class TaskC {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int [] [] mas = new int[n][n];
+
+        step1(n);
+        step2(mas);
+        step3(mas);
+
+
+    }
+    private static int[ ][ ] step1(int n) {
+        boolean max = false;
+        boolean min = false;
+        int [][] mas = new int[n][n];
+        do {
+            for (int i = 0; i < mas.length; i++) {
+                for (int j = 0; j < mas.length; j++) {
+                    mas[i][j] = (int) (Math.random() * (2 * n + 1)) - n;
+                    if (mas[i][j] == -n) min = true;
+                    if (mas[i][j] == n) max = true;
+                }
+            }
+        } while (!max && !min);
+            for (int i = 0; i < mas.length; i++) {
+                for (int j = 0; j < mas.length; j++) {
+                    System.out.print(mas[i][j] + " ");
+                }
+                System.out.println();
+            }
+        return mas;
+    }
+
+    static int step2(int[ ][ ] mas){
+        return 0;
+
+    }
+
+    static int[ ][ ] step3(int[ ][ ] mas){
+return null;
+    }
+
+}
