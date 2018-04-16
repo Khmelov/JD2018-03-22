398e9d79b9421d68c391536718e4d0075d929e23
svolchek <6252750@gmail.com>
2018-04-16 15:18:53 +0300

    jd01_11

--- src/by/it/volchek/jd01_11/ListB.java
+++ src/by/it/volchek/jd01_11/ListB.java
@@ -3,34 +3,13 @@ package by.it.volchek.jd01_11;
 import java.util.*;
 
 /**
- * Created by volchek on 14.04.18.
+ * Created by volchek on 16.04.18.
  */
-public class ListA<T> implements List<T> {
+public class ListB<T> implements List<T>{
     @SuppressWarnings("all")
     private T[] elements = (T[])new Object[]{};
     private int size = 0;
 
-
-
-    @Override
-    public boolean add(T element) {
-        if(size==elements.length)
-            elements=Arrays.copyOf(elements,(size*3/2+1));
-        elements[size++] = element;
-        return false;
-    }
-    @Override
-    public T remove(int index) {
-        T deleted = elements[index];
-        System.arraycopy(elements,index+1,elements,index,size-index);
-        size--;
-        return deleted;
-    }
-    @Override
-    public T get(int index) {
-
-        return elements[index];
-    }
     @Override
     public  String toString(){
         StringBuilder sb = new StringBuilder("[");
@@ -43,9 +22,6 @@ public class ListA<T> implements List<T> {
         sb.append("]");
         return sb.toString();
     }
-
-
-
 
     @Override
     public int size() {
@@ -77,7 +53,13 @@ public class ListA<T> implements List<T> {
         return null;
     }
 
-
+    @Override
+    public boolean add(T element) {
+        if(size==elements.length)
+            elements= Arrays.copyOf(elements,(size*3/2+1));
+        elements[size++] = element;
+        return true;
+    }
 
     @Override
     public boolean remove(Object o) {
@@ -91,7 +73,20 @@ public class ListA<T> implements List<T> {
 
     @Override
     public boolean addAll(Collection<? extends T> c) {
-        return false;
+
+        if (c.isEmpty()) {
+            System.out.println("попытка добваить пустое множество");
+            return false;
+        } else {
+            Iterator<? extends T> iter = c.iterator();
+            while (iter.hasNext()) {
+                if (size == elements.length)
+                    elements = Arrays.copyOf(elements, (size * 3 / 2 + 1));
+                elements[size++] = iter.next();
+            }
+            return true;
+
+        }
     }
 
     @Override
@@ -114,19 +109,58 @@ public class ListA<T> implements List<T> {
 
     }
 
-
+    @Override
+    public T get(int index) {
+        return elements[index];
+    }
 
     @Override
     public T set(int index, T element) {
-        return null;
+        if (index>size) {
+            System.out.println("Введен неправильный индекс");
+
+            return element;
+        }
+        else {
+            T elem  = elements[index];
+            elements[index] = element;
+
+            return elem;
+        }
     }
 
     @Override
     public void add(int index, T element) {
+        if (index>=elements.length) {
+            System.out.println("Введен неправильный индекс, элемент будет добавлен в конец списка.");
+            add(element);
 
+        }
+        else {
+            size++;
+            T[] elementsArr =(T[])new Object[]{};
+            elementsArr = Arrays.copyOf(elements,elements.length);
+            if(size==elements.length)
+                elements=Arrays.copyOf(elements,(size*3/2+1));
+            for (int i = 0; i < index ; i++) {
+                elements[i] = elementsArr[i];
+            }
+            elements[index] = element;
+            for (int i = index+1; i <size ; i++) {
+                elements[i]=elementsArr[i-1];
+            }
+
+
+        }
     }
 
-
+    @Override
+    public T remove(int index) {
+        T deleted = elements[index];
+        System.arraycopy(elements,index+1,elements,index,size-index);
+        size--;
+        return deleted;
+    }
 
     @Override
     public int indexOf(Object o) {
@@ -153,100 +187,3 @@ public class ListA<T> implements List<T> {
         return null;
     }
 }
-/* @SuppressWarnings("all")
-    private T[] elements = (T[])new Object[]{};
-    private int size = 0;
-
-
-    @Override
-    public boolean add(T element) {
-        if(size==elements.length)
-            elements=Arrays.copyOf(elements,(size*3/2+1));
-        elements[size++] = element;
-        return false;
-    }
-    @Override
-    public T remove(int index) {
-        T deleted = elements[index];
-        System.arraycopy(elements,index+1,elements,index,size-index);
-        size--;
-        return deleted;
-    }
-    @Override
-    public T get(int index) {
-
-        return elements[index];
-    }
-    @Override
-    public  String toString(){
-        StringBuilder sb = new StringBuilder("[");
-        String delimeter="";
-        for (int i = 0; i <size ; i++) {
-
-            sb.append(delimeter).append(elements[i]);
-            delimeter=", ";
-        }
-        sb.append("]");
-        return sb.toString();
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends T> c) {
-
-        if (c.isEmpty()) {
-            System.out.println("попытка добваить пустое множество");
-            return false;
-        } else {
-            Iterator<? extends T> iter = c.iterator();
-            while (iter.hasNext()) {
-                if (size == elements.length)
-                    elements = Arrays.copyOf(elements, (size * 3 / 2 + 1));
-                elements[size++] = iter.next();
-            }
-            return true;
-
-        }
-    }
-
-    @Override
-    public T set(int index, T element) {
-        if (index>size) {
-            System.out.println("Введен неправильный индекс");
-
-            return element;
-        }
-        else {
-            T elem  = elements[index];
-            elements[index] = element;
-
-            return elem;
-        }
-
-    }
-
-    @Override
-    public void add(int index, T element) {
-        if (index>=elements.length) {
-            System.out.println("Введен неправильный индекс, элемент будет добавлен в конец списка.");
-            add(element);
-
-        }
-        else {
-            size++;
-            T[] elementsArr =(T[])new Object[]{};
-            elementsArr = Arrays.copyOf(elements,elements.length);
-            if(size==elements.length)
-                elements=Arrays.copyOf(elements,(size*3/2+1));
-            for (int i = 0; i < index ; i++) {
-                elements[i] = elementsArr[i];
-            }
-            elements[index] = element;
-            for (int i = index+1; i <size ; i++) {
-                elements[i]=elementsArr[i-1];
-            }
-
-
-        }
-
-    }
-*/
