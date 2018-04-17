67a4d758ed2b4cf79e57bdbde912adf6283f70aa
demologin <375336849110@tut.by>
2018-04-17 12:33:09 +0300

    lesson12 classwork

---
+++ src/by/it/akhmelev/jd01_12/TaskA1.java
@@ -0,0 +1,31 @@
+package by.it.akhmelev.jd01_12;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class TaskA1 {
+
+    private List<Integer> grades=new ArrayList<>();
+
+    private void clearBad(List<Integer> grades){
+
+        Iterator<Integer> iterator = grades.iterator();
+        while (iterator.hasNext()) {
+            Integer value = iterator.next();
+            if (value < 4) iterator.remove();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        TaskA1 task=new TaskA1();
+        for (int i = 0; i < 25; i++) {
+            task.grades.add((int) (Math.random()*10+1));
+        }
+        System.out.println(task.grades);
+        task.clearBad(task.grades);
+        System.out.println(task.grades);
+    }
+
+}
