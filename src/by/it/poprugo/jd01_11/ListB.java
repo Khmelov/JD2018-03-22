0a9198317c6057513e643d67732a84ef0e8712c0
AlenaVP <poprugo@gmail.com>
2018-04-19 09:29:48 +0300

    A of 11 and 12

--- src/by/it/poprugo/jd01_11/ListB.java
+++ src/by/it/poprugo/jd01_11/ListB.java
@@ -2,9 +2,9 @@ package by.it.poprugo.jd01_11;
 
 import java.util.*;
 
-public class ListA<T> implements List<T> {
+public class ListB<T> implements List<T> {
 
-//    @SuppressWarnings("all")
+
     private T[] elements = (T[]) new Object[]{};
     private int size = 0;
 
@@ -18,57 +18,27 @@ public class ListA<T> implements List<T> {
 
     @Override
     public boolean remove(Object o) {
-        int index=indexOf(o);
-        if (index>-1) remove(index);
-        return (index>-1);
-    }
-
-    @Override
-    public int indexOf(Object o) {
-        if (o == null) {
-            for (int i = 0; i < size; i++)
-                if (elements[i] == null)
-                    return i;
-        } else {
-            for (int i = 0; i < size; i++)
-                if (o.equals(elements[i]))
-                    return i;
-        }
-        return -1;
-    }
-
-    @Override
-    public void add(int index, T element) {
-        if (size == elements.length)
-            elements = Arrays.copyOf(elements, (size * 3) / 2 + 1);
-        System.arraycopy(elements, index, elements, index + 1, size - index);
-        elements[index] = element;
-        size++;
-    }
-
-    @Override
-    public T remove(int index) {
-        T del = elements[index];
-        System.arraycopy(elements, index + 1, elements, index, size - 1 - index);
-        size--;
-        return del;
+        return false;
     }
 
     @Override
     public T get(int index) {
-        return elements[index];
+        return null;
     }
 
     @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder("[");
-        String delimiter = "";
-        for (int i = 0; i < size; i++) {
-            sb.append(delimiter).append(elements[i]);
-            delimiter = ", ";
-        }
-        sb.append("]");
-        return sb.toString();
+    public T set(int index, T element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
     }
 
     @Override
@@ -107,11 +77,6 @@ public class ListA<T> implements List<T> {
     }
 
     @Override
-    public boolean addAll(Collection<? extends T> c) {
-        return false;
-    }
-
-    @Override
     public boolean addAll(int index, Collection<? extends T> c) {
         return false;
     }
@@ -132,8 +97,13 @@ public class ListA<T> implements List<T> {
     }
 
     @Override
-    public T set(int index, T element) {
+    public T remove(int index) {
         return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
     }
 
     @Override
