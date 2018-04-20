f606643a63410ba10d93750ab60250576d3c2a76
Michael <mishanya.v91@mail.ru>
2018-04-21 00:32:34 +0300

    jd01_13 A + calc A

---
+++ src/by/it/verishko/jd01_13/TaskB.java
@@ -0,0 +1,12 @@
+package by.it.verishko.jd01_13;
+
+import java.util.Scanner;
+
+public class TaskB {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str;
+
+
+    }
+}
