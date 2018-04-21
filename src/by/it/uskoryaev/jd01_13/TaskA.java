0d0bb664a8c951a0ae68d75d7ecdc76b65e66341
ProgrammStart <ProgrammStart625@gmail.com>
2018-04-21 09:39:57 +0300

    task 12 a task 13 a

--- src/by/it/uskoryaev/jd01_13/TaskA.java
+++ src/by/it/uskoryaev/jd01_13/TaskA.java
@@ -5,9 +5,18 @@ import java.util.HashMap;
 public class TaskA {
     public static void main(String[] args) {
         try {
-            if (Math.random()> 0.5)new HashMap<String,String>();
+            if (Math.random() > 0.5) new HashMap<String, String>(null);
             else Integer.parseInt("привет");
-        }catch (Exception e){
+        } catch (NumberFormatException | NullPointerException e) {
+            StackTraceElement[] stackTraceElements = e.getStackTrace();
+            for (StackTraceElement stackTraceElement : stackTraceElements) {
+                if (TaskA.class.getName().equals(stackTraceElement.getClassName())) {
+                    System.out.println("name: "+e.getClass().getName());
+                    System.out.println("class: "+stackTraceElement.getClassName());
+                    System.out.println("line: "+stackTraceElement.getLineNumber());
+                    break;
+                }
+            }
 
         }
     }
--- src/by/it/uskoryaev/jd01_13/TaskB.java
+++ src/by/it/uskoryaev/jd01_13/TaskB.java
@@ -1,4 +1,34 @@
 package by.it.uskoryaev.jd01_13;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class TaskB {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str;
+        List<Double> listNumb = new ArrayList<>();
+        double numCor=0;
+        try {
+            while (!(str = scanner.next()).equals("end")) {
+                double numb = Double.parseDouble(str);
+                System.out.println(numb);
+                listNumb.add(numb);
+                numCor+=numb;
+                System.out.println(Math.sqrt(numCor));
+            }
+
+        }catch (ArithmeticException | NumberFormatException e){
+            StackTraceElement[] stackTraceElements = e.getStackTrace();
+            for (StackTraceElement stackTraceElement : stackTraceElements) {
+                if (TaskB.class.getName().equals(stackTraceElement.getClassName())) {
+                    System.out.println("name: "+e.getClass().getName());
+                    System.out.println("class: "+stackTraceElement.getClassName());
+                    System.out.println("line: "+stackTraceElement.getLineNumber());
+                    break;
+                }
+            }
+        }
+    }
 }
