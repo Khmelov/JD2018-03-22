c1de01dc8e4909b7c0e129be9eadcea151bd00c0
Olga_Matyuta <tiefatem@gmail.com>
2018-04-21 09:30:44 +0300

    jd01...13

---
+++ src/by/it/matyuta/calc/ConsoleRunner.java
@@ -0,0 +1,24 @@
+package by.it.matyuta.calc;
+
+import java.util.Scanner;
+
+class ConsoleRunner {
+    public static void main(String[] args) {
+        Printer printer=new Printer();
+        Parser parser=new Parser();
+
+        Scanner scanner=new Scanner(System.in);
+        String line;
+
+        while (!(line=scanner.nextLine()).equals("end")){
+            try {
+                Var result=parser.calc(line);
+                printer.print(result);
+            }
+            catch (CalcException e){
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+}
