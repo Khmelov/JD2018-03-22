9f3e05b377536d6896d3d67465aa290159ea0957
Michael <mishanya.v91@mail.ru>
2018-04-24 12:16:42 +0300

    jd01_15 calc

--- src/by/it/verishko/calc/ConsoleRunner.java
+++ src/by/it/verishko/calc/ConsoleRunner.java
@@ -1,12 +1,28 @@
 package by.it.verishko.calc;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 class ConsoleRunner {
     public static void main(String[] args) {
-
         Printer printer = new Printer();
         Parser parser = new Parser();
+
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader(
+                        Util.getPathVarsTxt()
+                )
+        )) {
+            String line;
+            while ((line = reader.readLine()) != null)
+                parser.calc(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (CalcException e) {
+            System.out.println(e);
+        }
 
         Scanner sc = new Scanner(System.in);
         String line;
