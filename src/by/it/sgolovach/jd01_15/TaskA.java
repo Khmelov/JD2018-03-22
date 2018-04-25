300245d3af1b118ae447bb0e1ea502e692525ec6
SGolovach <sergei.golovach90@gmail.com>
2018-04-25 22:58:57 +0300

    jd01_14 Task C, jd01_15 Task A,B

--- src/by/it/sgolovach/jd01_15/TaskA.java
+++ src/by/it/sgolovach/jd01_15/TaskA.java
@@ -17,15 +17,16 @@ public class TaskA {
         return getPath(cl) + filename;
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         int[][] m = new int[6][4];
+        String path = getPath(TaskA.class, "matrix.txt");
 
-        String path=getPath(TaskA.class,"matrix.txt");
-        try(PrintWriter printer=new PrintWriter(new FileWriter(path))) {
-
+        try (PrintWriter printer = new PrintWriter(
+                new FileWriter(path))
+        ) {
             for (int i = 0; i < m.length; i++) {
                 for (int j = 0; j < m[i].length; j++) {
-                    printer.printf("%3d",(int)(-15+Math.random()*31));
+                    printer.printf("%3d ", (int) (-15 + Math.random() * 31));
                 }
                 printer.println();
             }
@@ -33,13 +34,19 @@ public class TaskA {
             e.printStackTrace();
         }
 
-        try(BufferedReader bufferedReader=new BufferedReader(new FileReader(path))) {
+        try (BufferedReader bufferedReader = new BufferedReader(
+                new FileReader(path))
+        ) {
             for (int i = 0; i < m.length; i++) {
+                String[] sarr = bufferedReader.readLine().trim().split("\\s+");
+                for (int j = 0; j < m[i].length; j++) {
+                    m[i][j] = Integer.parseInt(sarr[j]);
+                    System.out.printf("%3d ",m[i][j]);
+                }
+                System.out.println();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 }
