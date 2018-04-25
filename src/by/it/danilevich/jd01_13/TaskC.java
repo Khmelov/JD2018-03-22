b0dd27880c6d21f5a0c21acdc7a610de982d3aef
svetlana <sl.danilevich@gmail.com>
2018-04-26 02:32:29 +0300

    Danilevich jd01_14, 01_15 не до конца

--- src/by/it/danilevich/jd01_13/TaskC.java
+++ src/by/it/danilevich/jd01_13/TaskC.java
@@ -5,40 +5,36 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class TaskC {
-    private static String inputNumber;
-    private static int countErrors;
-    private static List<Double> numbers;
+    private static Scanner sc;
     public static void main(String[] args) throws InterruptedException {
-
-        numbers = new ArrayList<>();
-
-        countErrors = 0;
         Scanner sc = new Scanner(System.in);
-
-        while (countErrors < 5) {
-            inputNumber = sc.next();
-            readData();
-        }
+        readData();
     }
-
 
     private static void readData() throws InterruptedException {
         double rez;
+        List<Double> numbers = new ArrayList<>();
+        int countErrors = 0;
+        String inputNumber="";
+
+
         try {
+            while (countErrors < 5) inputNumber = sc.next();
+
             rez = Double.parseDouble(inputNumber);
             numbers.add(rez);
 
         } catch (NumberFormatException | NullPointerException | ArithmeticException ex) {
             countErrors ++;
-            if (countErrors==5) { throw new ArithmeticException();
+            Thread.sleep(100);
+            for (int i=numbers.size()-1; i >=0; i--) {
+                System.out.print(numbers.get(i) + " ");
+            }
+            System.out.println();
+            if (countErrors==5) {
+                throw ex;//new ArithmeticException();
               }
-            else {
-                Thread.sleep(100);
-                for (int i=numbers.size()-1; i >=0; i--) {
-                    System.out.print(numbers.get(i) + " ");
-                  }
-                System.out.println();
-           }
+
 
         }
     }
