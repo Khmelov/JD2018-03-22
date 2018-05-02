3bc74cdb8e2e7c001693a85c244e7daa39895b40
artsiomfando <vlaamse10@gmail.com>
2018-05-02 18:42:08 +0300

    Fando. jd.15 A

--- src/by/it/fando/jd01_15/TaskA.java
+++ src/by/it/fando/jd01_15/TaskA.java
@@ -17,38 +17,36 @@ public class TaskA {
         return getPath(cl)+filename;
     }
 
-//    public static void main(String[] args) {
-//
-//        int[][] m = new int[6][4];
-//        String path = getPath(TaskA.class, "matrix.txt");
-//
-//        try (PrintWriter printer = new PrintWriter(
-//                new FileWriter(path))
-//        ) {
-//            for (int i = 0; i < m.length; i++) {
-//                for (int j = 0; j < m[i].length; j++) {
-//                    m[i][j] = (int) (-15 + Math.random() * 31);
-//                    printer.printf("%3d ", m[i][j]);
-//                }
-//                printer.println();
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        try (BufferedReader bufferedReader = new BufferedReader(
-//                new FileReader(path))
-//        ) {
-//            for (int i = 0; i < m.length; i++) {
-//                String[] sarr = bufferedReader.readLine().trim().split("\\s+");
-//                for (int j = 0; j < m[i].length; j++) {
-//                    m[i][j] = Integer.parseInt(sarr);
-//                    System.out.printf("%3d ", m[i][j]);
-//                }
-//                System.out.println();
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    public static void main(String[] args) {
+        int[][] m = new int[6][4];
+        String path = getPath(TaskA.class, "matrix.txt");
+
+        try (PrintWriter printer = new PrintWriter(
+                new FileWriter(path))
+        ) {
+            for (int i = 0; i < m.length; i++) {
+                for (int j = 0; j < m[i].length; j++) {
+                    printer.printf("%3d ", (int) (-15 + Math.random() * 31));
+                }
+                printer.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedReader bufferedReader = new BufferedReader(
+                new FileReader(path))
+        ) {
+            for (int i = 0; i < m.length; i++) {
+                String[] sarr = bufferedReader.readLine().trim().split("\\s+");
+                for (int j = 0; j < m[i].length; j++) {
+                    m[i][j] = Integer.parseInt(sarr[j]);
+                    System.out.printf("%3d ", m[i][j]);
+                }
+                System.out.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
