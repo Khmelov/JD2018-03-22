9d493183a5b4ba9df28a89f36277b2508bdd0c90
Michael <mishanya.v91@mail.ru>
2018-05-04 23:59:50 +0300

    jd02_01

---
+++ src/by/it/verishko/jd02_01/IBuyer.java
@@ -0,0 +1,7 @@
+package by.it.verishko.jd02_01;
+
+public interface IBuyer {
+    void enterToMarket(); //вошел в магазин (мгновенно)
+    void chooseGoods(); //выбрал товар (от 0,5 до 2 секунд)
+    void goOut(); //отправился на выход(мгновенно)
+}
