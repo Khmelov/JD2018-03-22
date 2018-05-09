50f45c136e852cf1f8c43258ebe38aefacbd6150
ProgrammStart <ProgrammStart625@gmail.com>
2018-05-09 20:18:42 +0300

    Task jd2_03 part the time

--- src/by/it/uskoryaev/jd02_03/Runner.java
+++ src/by/it/uskoryaev/jd02_03/Runner.java
@@ -1,36 +1,41 @@
-package by.it.uskoryaev.jd02_02;
+package by.it.uskoryaev.jd02_03;
 
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Runner {
-    private static List<Thread> cashiers = new ArrayList<>();
+    private static List<Thread> buyersThread = new ArrayList<>();
+
 
     public static void main(String[] args) {
+
+        ExecutorService service = Executors.newFixedThreadPool(5);
         for (int i = 1; i <= 5; i++) {
-            Thread cashier = new Thread(new Cashier(i));
-            cashiers.add(cashier);
-            cashier.start();
+            service.execute(new Cashier(i));
         }
         while (!Dispatcher.allBuyersInShop()) {
             Util.sleep(1000);
             int count = Util.random(2);
             for (int i = 1; i <= count; i++) {
-                if (Dispatcher.allBuyersInShop())
+                if (Dispatcher.allBuyersInShop()) {
                     break;
+                }
                 Buyer buyer = Dispatcher.addNewBuyer();
                 buyer.start();
             }
         }
-        for (Thread cashier : cashiers) {
+        for (Thread buyer : buyersThread) {
             try {
-                cashier.join();
+                buyer.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
         Thread.yield();
         System.out.println("Магазин закрылся");
+        service.shutdown();
     }
 }
