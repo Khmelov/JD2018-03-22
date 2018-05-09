7df7558efd253b31b8fc71d11be0fa8d8ad9e317
eukovko <eu.kovko@gmail.com>
2018-05-09 23:34:36 +0300

    jd02_03 A,B,C

---
+++ src/by/it/kovko/jd02_03/Pensioner.java
@@ -0,0 +1,10 @@
+package by.it.kovko.jd02_03;
+
+public class Pensioner extends ConcreteCustomer {
+    Pensioner(int i, Shop shop) {
+        super(i, shop);
+        setTimeFactor(1.5);
+        setName(getName()+" (пенсионер)");
+        setPriority(2);
+    }
+}
