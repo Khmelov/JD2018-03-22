84526d613058fa2f7dd21a3b57de5c1f9d2963f4
Sdanilevich <sl.danilevich@gmail.com>
2018-05-10 00:52:13 +0300

    jd02_02 все равно не то что надо

--- src/by/it/danilevich/jd02_02/QueueBuyer.java
+++ src/by/it/danilevich/jd02_02/QueueBuyer.java
@@ -1,27 +1,52 @@
 package by.it.danilevich.jd02_02;
 
+import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.ListIterator;
 
 class QueueBuyer {
     private static LinkedList<Buyer> queue = new LinkedList<>();
+    private String name;
+
+    public QueueBuyer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     void addbuyer(Buyer buyer){
-        queue.addLast(buyer);
+        synchronized (queue) {
+            String s = "Очередь " + getName() + getSize()+" чел.\n";
+            queue.addLast(buyer);
+            //LinkedList<Buyer> queue1 = getQueue();
+           // printQueue(s + buyer.getNameBuyer() + " встал в очередь " + getName(), queue);
+        }
     }
     public int getSize(){
         return queue.size();
     }
 
     Buyer extractByuer(){
-        if (queue.size()!=0){
-            return queue.pollFirst();
+        synchronized (queue) {
+            if (queue.size() != 0) {
+                return queue.pollFirst();
+            }
+            return null;
         }
-        return null;
     }
     void removeByuer(Buyer buyer){
-        if (queue.size()!=0){
-            queue.remove(buyer);
+        synchronized (queue) {
+            if (queue.size() != 0) {
+                queue.remove(buyer);
+                String s = "Очередь " + getName() + getSize()+" чел.\n";
+              //  LinkedList<Buyer> queue1 = getQueue();
+             //   printQueue(buyer + " удалили из очереди "+getName(),queue);
+            }
         }
+    }
+    private static LinkedList<Buyer> getQueue(){
+        return queue;
     }
     public boolean contains(Buyer buyer){
         return queue.contains(buyer);
@@ -29,22 +54,15 @@ class QueueBuyer {
 
 
 
-    static void printQueue(){
-        String queuqPrint="";
-        ListIterator<Buyer> list = queue.listIterator();
-        if (list.hasNext()) queuqPrint = "Общая очередь ";
-        while (list.hasNext()){
-            Buyer elem =list.next();
-            queuqPrint = queuqPrint.concat(elem.getNameBuyer()) +" ";
-        }
-        System.out.println(queuqPrint);
-    }
+    public void printQueue(String addString,LinkedList<Buyer> queue1){
+            String queuqPrint = "";
+            Iterator<Buyer> list = queue1.listIterator();
+            queuqPrint = "Очередь "+getName() + queue1.size() +"чел.:";
+            while (list.hasNext()) {
+                Buyer elem = list.next();
+                queuqPrint = queuqPrint.concat(elem.getNameBuyer()) + " ";
+            }
+            System.out.println(addString+ "\n" +queuqPrint);
+      }
 
-    static Buyer getFirst(){
-        ListIterator<Buyer> list = queue.listIterator();
-        if (list.hasNext()){
-            return list.next();
-         }
-         return null;
-    }
 }
