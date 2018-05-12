973bc446ce2396e27ceb35f791fd729596d1a0e2
nikitaromankov <nikitarom2@gmail.com>
2018-05-12 12:00:48 +0300

    jd02_05

---
+++ src/by/it/romankov/jd02_05/Messages.java
@@ -0,0 +1,11 @@
+package by.it.romankov.jd02_05;
+
+public interface Messages {
+
+        String WELCOME = "msg.welcome";
+    String NAME = "msg.myname";
+    String HOWDOYUODO = "msg.goedoyuodo";
+
+
+
+}
---
+++ src/by/it/romankov/jd02_05/ResousreManager.java
@@ -0,0 +1,11 @@
+package by.it.romankov.jd02_05;
+
+import java.util.ResourceBundle;
+
+public enum ResousreManager {
+    INSTANCE;
+private ResourceBundle resourceBundle;
+    ResousreManager() {
+
+    }
+}
