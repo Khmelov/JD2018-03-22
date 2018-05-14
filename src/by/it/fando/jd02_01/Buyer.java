0ce959ba8c4b908aa1fcaf0d8467202e92f43e63
artsiomfando <vlaamse10@gmail.com>
2018-05-14 20:07:02 +0300

    2

--- src/by/it/fando/jd02_01/Buyer.java
+++ src/by/it/fando/jd02_01/Buyer.java
@@ -1,19 +1,8 @@
 package by.it.fando.jd02_01;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Buyer extends Thread implements IBuyer, IUseBasket {
     private String name;
     public Buyer (int number) { name = "Покупатель №" + number; }
-    HashMap<String, Double> products = new HashMap<>(); {
-        products.put("Хлеб",1.1);
-        products.put("Молоко",2.2);
-        products.put("Сахар",3.3);
-        products.put("Соль",4.4);
-        products.put("Масло",5.5);
-    }
-    Map.Entry[] array = products.entrySet().toArray(new Map.Entry[0]);
 
     @Override
     public String toString() {
@@ -49,7 +38,7 @@ public class Buyer extends Thread implements IBuyer, IUseBasket {
 
     @Override
     public void takeBasket() {
-        Util.sleep(Util.random(50,100));
+        Util.sleep(Util.random(100,200));
         System.out.println(this+"взял корзину");
     }
 
@@ -58,10 +47,11 @@ public class Buyer extends Thread implements IBuyer, IUseBasket {
         int r = Util.random(1,4);
         for ( int i = 1; i <=r ; i++ ) {
             Util.sleep(Util.random(100, 200));
-            int w = Util.random(0, array.length-1);
-            Object key = array[w].getKey();
-            Object value = array[w].getValue();
-            System.out.println(this+"Положил товар " + key +" (" + value + ") в корзину.");
+            int w = Util.random(0, Goods.pairs.size()-1);
+            String key = Goods.pairs.get(w).getKey();
+            Double value = Goods.pairs.get(w).getValue();
+
+            System.out.println(this+"Положил товар " + key +" (" + value + ") в корзину");
         }
     }
 
--- src/by/it/fando/jd02_01/Goods.java
+++ src/by/it/fando/jd02_01/Goods.java
@@ -1,17 +1,20 @@
 package by.it.fando.jd02_01;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Goods {
 
-    HashMap<String, Double> products = new HashMap<>(); {
-        products.put("Хлеб",1.1);
-        products.put("Молоко",2.2);
-        products.put("Сахар",3.3);
-        products.put("Соль",4.4);
-        products.put("Масло",5.5);
-    }
-    Map.Entry[] array = products.entrySet().toArray(new Map.Entry[0]);
+    static HashMap<String, Double> products = new HashMap<>() {
+        {
+            this.put("хлеб", 1.1);
+            this.put("молоко", 2.2);
+            this.put("сахар", 3.3);
+            this.put("соль", 4.4);
+            this.put("масло", 5.5);
+        }
+    };
+    static ArrayList<Map.Entry<String, Double>> pairs = new ArrayList<>(products.entrySet());
 
 }
