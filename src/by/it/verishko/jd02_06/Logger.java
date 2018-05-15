d78d8427e5e56084da31e5fe89df9e1a32c18587
Michael <mishanya.v91@mail.ru>
2018-05-15 12:25:10 +0300

    jd02_06 A

--- src/by/it/verishko/jd02_06/Logger.java
+++ src/by/it/verishko/jd02_06/Logger.java
@@ -2,30 +2,38 @@ package by.it.verishko.jd02_06;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 class Logger {
 
     private String filename;
 
-    private static Logger instance;
-
-    private Logger(String filename) {
+    private Logger() {
         filename = getPath(Logger.class, "log.txt");
     }
+
+    private static Logger instance;
 
     static Logger getLogger() {
         if (instance == null) {
             synchronized (Logger.class) {
-//                if (instance == null)
-//                    instance = new Logger();
+                if (instance == null)
+                    instance = new Logger();
             }
         }
         return instance;
     }
 
     void toLog(String message) {
-//        try (PrintWriter printWriter = new PrintWriter(new FileWriter()));
+        try (PrintWriter printWriter =
+                     new PrintWriter(
+                             new FileWriter(filename, true))
+        ) {
+            printWriter.println(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static String getPath(Class<?> cl) {
