b5eac4717f3122f9c2a69151c199916a6dc61ec6
Ksusha Kirova <kirova.ksu@gmail.com>
2018-05-17 00:57:57 +0300

    Выполненные задания (jd02-06 ABC)

--- src/by/it/kirova/calc/Logger.java
+++ src/by/it/kirova/calc/Logger.java
@@ -1,39 +1,63 @@
 package by.it.kirova.calc;
 
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Logger {
 
-    static String loggerPath = null;
-    static Queue<String> messageBuffer = new LinkedList<>();
-    static int messageId = 0;
+    String loggerPath = null;
+    Queue<String> messageBuffer = new LinkedList<>();
+    int messageId = 0;
+    private static Logger instance;
+
+    static Logger getLogger() {
+        if (instance == null)
+            synchronized (Logger.class) {
+                if (instance == null)
+                    instance = new Logger();
+            }
+        return instance;
+    }
+
+    private Logger() {
+        loggerPath = Util.getLogPath();
+    }
 
     public enum MessageType {
         USER_INPUT, CALC_OUTPUT, ERROR_MESSAGE
     }
 
-    public static void setLoggerPath(String path) {
-        loggerPath = path;
+    public void println(String message, MessageType type) {
+        println_impl(message, type, true);
     }
 
-    public static void println(String message, MessageType type) {
+    public void println_impl(String message, MessageType type, boolean addToReport) {
         switch (type) {
             case USER_INPUT:
+                if(addToReport)
+                    BaseReport.getReport().addOperation(type + "\t" + message);
                 break;
             case CALC_OUTPUT:
+                if(addToReport)
+                    BaseReport.getReport().addOperation(type + "\t" + message);
                 System.out.println(message);
                 break;
             case ERROR_MESSAGE:
+                if(addToReport)
+                    BaseReport.getReport().addOperation(type + "\t" + message);
                 System.err.println(message);
                 break;
         }
 
+        printToFile(message, type);
+    }
+
+    void printToFile(String message, MessageType type){
         if (loggerPath == null)
             return;
 
@@ -51,15 +75,19 @@ public class Logger {
         }
     }
 
-    public static void printErrorMessage(Exception e) {
-        println(e.getMessage(), MessageType.ERROR_MESSAGE);
+    public void printErrorMessage(Exception e) {
+        BaseReport.getReport().addException(e);
+        println_impl(e.getMessage(), MessageType.ERROR_MESSAGE, false);
     }
 
-    public static void printErrorStackTrace(Exception e) {
+    public void printErrorStackTrace(Exception e) {
+        BaseReport.getReport().addException(e);
+
         StringBuilder sb = new StringBuilder();
         for (StackTraceElement stackTraceElement : e.getStackTrace()) {
             sb.append(stackTraceElement).append("\n");
         }
-        println(sb.toString(), MessageType.ERROR_MESSAGE);
+        println_impl(sb.toString(), MessageType.ERROR_MESSAGE, false);
     }
+
 }
---
+++ src/by/it/kirova/jd02_06/TaskA.java
@@ -0,0 +1,11 @@
+package by.it.kirova.jd02_06;
+
+public class TaskA {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger();
+        for (int i = 0; i <10 ; i++) {
+            logger.toLog("Message № " + i);
+        }
+
+    }
+}
