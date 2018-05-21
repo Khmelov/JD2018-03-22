cda695d6d1e588896a5d1e7fa300bbe0dcaeb7d1
Ksusha Kirova <kirova.ksu@gmail.com>
2018-05-22 01:19:09 +0300

    Выполненные задания (jd02-08 ABC)

--- src/by/it/kirova/jd02_08/DOMParser.java
+++ src/by/it/kirova/jd02_08/DOMParser.java
@@ -1,15 +1,11 @@
 package by.it.kirova.jd02_08;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
 import java.io.IOException;
 
 public class DOMParser {
@@ -28,17 +24,27 @@ public class DOMParser {
 
     private static void printer(Node node) {
 
-        if (node.getNodeType() == Node.ELEMENT_NODE)
-            System.out.println(tab + "<" + node.getNodeName() + ">");
+        if (node.getNodeType() == Node.ELEMENT_NODE) {
+            System.out.print(tab + "<" + node.getNodeName());
+            if (node.hasAttributes()) {
+                NamedNodeMap attr = node.getAttributes();
+                for (int i = 0; i < attr.getLength(); i++) {
+                    Node attribute = attr.item(i);
+                    System.out.print(" "+attribute.getNodeName()+"=\""+attribute.getNodeValue()+"\"");
+                }
+            }
+            System.out.println(">");
+        }
         if ((node.getNodeType() == Node.TEXT_NODE) && (!node.getNodeValue().trim().isEmpty()))
             System.out.println(tab + node.getNodeValue().trim());
 
         tab = tab + "\t";
 
-        NodeList child= node.getChildNodes();
+        NodeList child = node.getChildNodes();
         for (int i = 0; i < child.getLength(); i++) {
             printer(child.item(i));
         }
+
 
         tab = tab.substring(1);
 
