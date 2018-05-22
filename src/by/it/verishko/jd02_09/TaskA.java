66ac8d4fe17c29358647cefec73f89d9f2d2fca2
Michael <mishanya.v91@mail.ru>
2018-05-23 02:00:55 +0300

    jd02_08 C

---
+++ src/by/it/verishko/jd02_09/TaskA.java
@@ -0,0 +1,34 @@
+package by.it.verishko.jd02_09;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.*;
+
+public class TaskA {
+    public static void main(String[] args) {
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(Persons.class);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            String xmlSource = System.getProperty("user.dir") + "/src/by/it/verishko/jd02_07/Store/Store+XSD.xml";
+            String xmlTarget = System.getProperty("user.dir") + "/src/by/it/verishko/jd02_07/Store/Store+XSD.xml";
+
+            Persons persons = (Persons) unmarshaller.unmarshal(new File(xmlSource));
+
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            StringWriter out = new StringWriter();
+            marshaller.marshal(persons, System.out);
+            System.out.println(out.toString());
+            try (
+                    OutputStream outputStream = new FileOutputStream(xmlTarget)) {
+                marshaller.marshal(persons, outputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+}
