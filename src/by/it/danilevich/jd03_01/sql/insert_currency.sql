c6ab3ac01a2b425745e6024987180114f6ff0187
Sdanilevich <sl.danilevich@gmail.com>
2018-05-26 08:36:39 +0300

    jd03_01  create table подчеркивает красным

---
+++ src/by/it/danilevich/jd03_01/sql/insert_currency.sql
@@ -0,0 +1,4 @@
+INSERT INTO `danilevich`.`currency` (`id`, `name`) VALUES (1, 'BYB');
+INSERT INTO `danilevich`.`currency` (`id`, `name`) VALUES (2, 'USD');
+INSERT INTO `danilevich`.`currency` (`id`, `name`) VALUES (3, 'EUR');
+INSERT INTO `danilevich`.`currency` (`id`, `name`) VALUES (4, 'RUR');
