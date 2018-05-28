c6189ad8f7ee5dd14716b2c99480bd3d8cd3af32
Michael <mishanya.v91@mail.ru>
2018-05-29 01:22:20 +0300

    jd 03_02

--- src/by/it/verishko/jd03_02/Runner.java
+++ src/by/it/verishko/jd03_02/Runner.java
@@ -4,12 +4,41 @@ import java.sql.SQLException;
 
 public class Runner {
     public static void main(String[] args) throws SQLException {
-        User user=new User(-1,"llll","pppp","eeeee",2);
-        CRUD.createUser(user);
-        user.setLogin("wewwwww");
-        CRUD.updateUser(user);
-        CRUD.deleteUser(user);
-        user=CRUD.readUser(1);
+
+        System.out.println("Create + Read + Delete Role");
+        Role role = new Role(-1, "testRole");
+        RoleCRUD.create(role);
+        role.setRole("testRoleForDelete");
+        RoleCRUD.update(role);
+        RoleCRUD.delete(role);
+        System.out.println(role);
+        role = RoleCRUD.read(1);
+        System.out.println(role + "\n");
+
+        System.out.println("Create + Read + Delete User");
+        User user = new User(-1, "testUser", "testUserPass", "testUserEmail", 2);
+//        UserCRUD.create(user);
+        user.setLogin("testUserForDelete");
+        UserCRUD.update(user);
+        UserCRUD.delete(user);
         System.out.println(user);
+        user = UserCRUD.read(1);
+        System.out.println(user + "\n");
+
+        System.out.println("Create + Read + Delete Product");
+        Product product = new Product(
+                -1,
+                "Valera",
+                "neighbour",
+                777,
+                2
+        );
+//        ProductCRUD.create(product);
+        product.setDescription("testAdForDelete");
+//        ProductCRUD.update(product);
+        ProductCRUD.delete(product);
+        System.out.println(product);
+        product = ProductCRUD.read(1);
+        System.out.println(product + "\n");
     }
 }
