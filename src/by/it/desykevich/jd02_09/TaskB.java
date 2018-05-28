88a1f8bee941f940620ed4538c348d7ed26aba72
Desukevich <sergei_364@tut.by>
2018-05-28 16:40:06 +0300

    jd03-01  Desiukevich

--- src/by/it/desykevich/jd02_09/TaskB.java
+++ src/by/it/desykevich/jd02_09/TaskB.java
@@ -11,35 +11,35 @@ import javax.xml.bind.Unmarshaller;
 import java.io.*;
 
 public class TaskB {
-//    public static void main(String[] args) {
-//        try {
-//            JAXBContext jaxbContext = JAXBContext.newInstance(Users.class);
-//            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-//            String xmlSource = System.getProperty("user.dir") + "/src/by/it/desykevich/jd02_07/Users+XSD.xml";
-//            String xmlTarget=System.getProperty("user.dir")+"/src/by/it/desykevich/jd02_09/Users+XSD.xml";
-//            Users persons = (Users) unmarshaller.unmarshal(new File(xmlSource));
-//
-//            Gson gson=new GsonBuilder().serializeNulls().setPrettyPrinting().create();
-//            String json = gson.toJson(persons);
-//            System.out.println(json);
-//            Users persons1 = gson.fromJson(json, Users.class);
-//
-//            Marshaller marshaller = jaxbContext.createMarshaller();
-//            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-//
-//            StringWriter out=new StringWriter();
-//            marshaller.marshal(persons1, out);
-//            System.out.println(out.toString());
-//
-//            try (OutputStream outputStream=new FileOutputStream(xmlTarget)){
-//                marshaller.marshal(persons1, outputStream);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//
-//        } catch (JAXBException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
+    public static void main(String[] args) {
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(Users.class);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            String xmlSource = System.getProperty("user.dir") + "/src/by/it/desykevich/jd02_07/Users+XSD.xml";
+            String xmlTarget=System.getProperty("user.dir")+"/src/by/it/desykevich/jd02_09/Users+XSD.xml";
+            Users users = (Users) unmarshaller.unmarshal(new File(xmlSource));
+
+            Gson gson=new GsonBuilder().serializeNulls().setPrettyPrinting().create();
+            String json = gson.toJson(users);
+            System.out.println(json);
+            Users users1 = gson.fromJson(json, Users.class);
+
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            StringWriter out=new StringWriter();
+            marshaller.marshal(users1, out);
+            System.out.println(out.toString());
+
+            try (OutputStream outputStream=new FileOutputStream(xmlTarget)){
+                marshaller.marshal(users1, outputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
