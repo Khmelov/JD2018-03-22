13bfb6ccf44902824d61aefd96b609fc1d5c6a60
Michael <mishanya.v91@mail.ru>
2018-06-07 12:48:51 +0300

    jd03_07 pattern fix

--- src/by/it/verishko/project/java/controller/Util.java
+++ src/by/it/verishko/project/java/controller/Util.java
@@ -16,12 +16,18 @@ public class Util {
         return null;
     }
 
-    static String getString(String value, String regxp) throws ParseException {
-        if (value.equals(regxp)) return value;
-        else throw new ParseException("Input error", 1);
+//    static String getString(String value, String regxp) throws ParseException {
+//        if (value.equals(regxp)) return value;
+//        else throw new ParseException("Input error", 1);
+//    }
+
+    static String getString(String value, String regxp) {
+        if (value == null) return null;
+        else if (value.matches(regxp)) return value;
+        else return null;
     }
 
-    static boolean isPost(HttpServletRequest req){
+    static boolean isPost(HttpServletRequest req) {
         return req.getMethod().equalsIgnoreCase("post");
     }
 }
