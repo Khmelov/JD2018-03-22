cfb97d0465dde1654a2c16cfd3ee95e62079fcbf
Ksusha Kirova <kirova.ksu@gmail.com>
2018-06-09 11:41:13 +0300

    Выполненные задания (jd03-07 ABC)

--- src/by/it/kirova/project/java/dao/connect/ConnectionCreator.java
+++ src/by/it/kirova/project/java/dao/connect/ConnectionCreator.java
@@ -1,40 +1,59 @@
 package by.it.kirova.project.java.dao.connect;
 
-import com.mysql.fabric.jdbc.FabricMySQLDriver;
+//import com.mysql.fabric.jdbc.FabricMySQLDriver;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
+//import java.sql.Driver;
+//import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionCreator {
-
-    static private Connection connection;
+    // для MySQL only
+    private static DataSource ds;
 
     static {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Driver driver = new FabricMySQLDriver();
-            DriverManager.registerDriver(driver);
-        } catch (SQLException | ClassNotFoundException e) {
+            InitialContext initialContext = new InitialContext();
+            ds = (DataSource) initialContext.lookup("java:/comp/env/jdbc/my_sql_kirova");
+
+        } catch (NamingException e) {
             e.printStackTrace();
         }
     }
 
-    private ConnectionCreator() {
-
-    }
-
     public static Connection getConnection() throws SQLException {
-        if (connection == null || connection.isClosed()) {
-            synchronized ((ConnectionCreator.class)) {
-                if (connection == null || connection.isClosed()) {
-                    connection = DriverManager.getConnection(CN.URL_DB, CN.USERDB, CN.PASSWORD);
-                }
-            }
-        }
-        return connection;
+        return ds.getConnection();
     }
-
+//    для JAVA
+//    static private Connection connection;
+//
+//    static {
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//            Driver driver = new FabricMySQLDriver();
+//            DriverManager.registerDriver(driver);
+//        } catch (SQLException | ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private ConnectionCreator() {
+//
+//    }
+//
+//    public static Connection getConnection() throws SQLException {
+//        if (connection == null || connection.isClosed()) {
+//            synchronized ((ConnectionCreator.class)) {
+//                if (connection == null || connection.isClosed()) {
+//                    connection = DriverManager.getConnection(CN.URL_DB, CN.USERDB, CN.PASSWORD);
+//                }
+//            }
+//        }
+//        return connection;
+//    }
+//
 
 }
