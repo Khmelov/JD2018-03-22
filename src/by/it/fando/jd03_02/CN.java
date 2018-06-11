6e51549a1cab8e4ed1bf272d583106b4f676a949
artsiomfando <vlaamse10@gmail.com>
2018-06-11 21:04:40 +0300

    Fando. jd.03_02

---
+++ src/by/it/fando/jd03_02/CN.java
@@ -0,0 +1,8 @@
+package by.it.fando.jd03_02;
+
+interface CN {
+    String URL_DB="jdbc:mysql://127.0.0.1:2016/fando"+
+            "?useUnicode=true&characterEncoding=UTF-8";
+    String USERDB="root";
+    String PASSWORD="";
+}
